3f33c32ab5c83a38280c9677e428e6124bb5e10b
JamesKinler <james193722@yahoo.com>
2023-03-31 09:49:07 -0700

    Update header.php

--- wp-content/themes/westcoast_nut_child/header.php
+++ wp-content/themes/westcoast_nut_child/header.php
@@ -9,42 +9,42 @@
   <link rel="stylesheet" href="http://fonts.googleapis.com/icon?family=Material+Icons"/>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900"/>
   <title>West Coast Nut</title>
+  <?php wp_head();?>
 </head>
-
-<?php wp_head();?>
-<header>
-  <div class="container-fluid leadboard-nav">
-    <div class="row">
-      <div class="col-lg-5 col-md-12">
-        <img class="img-fluid logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/white-wcn-logo.png" alt=""/></div>
-      <div class="col-lg-6 col-md-12">
-        <?php echo do_shortcode('[top_ad]');?></div>
-    </div>
-  </div>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light mynav">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toogle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <!-- <div class="collapse navbar-collapse" id="navbarContent"> -->
-      <?php
-          wp_nav_menu( array(
-              'menu' => 'Main Nav',
-              'theme_location'	=> 'primary menu',
-              'depth'				=> 3, // 1 = with dropdowns, 0 = no dropdowns.
-            'container'			=> 'div',
-            'container_class'	=> 'collapse navbar-collapse',
-            'container_id'		=> 'navbarContent',
-            'menu_class'		=> 'navbar-nav mr-auto',
-              'fallback_cb'		=> 'WP_Bootstrap_Navwalker::fallback',
-              'walker'			=> new WP_Bootstrap_Navwalker()
-          ));
-        ?>
-      <div class="justify-content-end ">
-        <ul class="navbar-nav flex-row">
-          <li class="nav-item"><a class="nav-link" href="#"><i class="fab fa-facebook-f"></i></a></li>
-          <li class="nav-item"><a class="nav-link" href="#"> <i class="fab fa-twitter"></i></a></li>
-        </ul>
+<body>
+  <header>
+    <div class="container-fluid leadboard-nav">
+      <div class="row">
+        <div class="col-lg-5 col-md-12">
+          <img class="img-fluid logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/white-wcn-logo.png" alt=""/></div>
+        <div class="col-lg-6 col-md-12">
+          <?php echo do_shortcode('[top_ad]');?></div>
       </div>
-    <!-- </div> -->
-  </nav>
-</header>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mynav">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toogle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <!-- <div class="collapse navbar-collapse" id="navbarContent"> -->
+        <?php
+            wp_nav_menu( array(
+                'menu' => 'Main Nav',
+                'theme_location'	=> 'primary menu',
+                'depth'				=> 3, // 1 = with dropdowns, 0 = no dropdowns.
+              'container'			=> 'div',
+              'container_class'	=> 'collapse navbar-collapse',
+              'container_id'		=> 'navbarContent',
+              'menu_class'		=> 'navbar-nav mr-auto',
+                'fallback_cb'		=> 'WP_Bootstrap_Navwalker::fallback',
+                'walker'			=> new WP_Bootstrap_Navwalker()
+            ));
+          ?>
+        <div class="justify-content-end ">
+          <ul class="navbar-nav flex-row">
+            <li class="nav-item"><a class="nav-link" href="#"><i class="fab fa-facebook-f"></i></a></li>
+            <li class="nav-item"><a class="nav-link" href="#"> <i class="fab fa-twitter"></i></a></li>
+          </ul>
+        </div>
+      <!-- </div> -->
+    </nav>
+  </header>
